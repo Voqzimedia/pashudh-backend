5494b82725a320d8330d27b22e8b8bc43adf7db0
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-04-28 23:15:19 +0530

    Testimonial added

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
-
-  if(env('NODE_ENV') === 'production'){
+  if (env("NODE_ENV") === "production") {
     const config = parse(process.env.DATABASE_URL);
     return {
-      defaultConnection: 'default',
+      defaultConnection: "default",
       connections: {
         default: {
-          connector: 'bookshelf',
+          connector: "bookshelf",
           settings: {
-            client: 'postgres',
+            client: "postgres",
             host: config.host,
             port: config.port,
             database: config.database,
@@ -22,22 +21,22 @@ module.exports = ({ env }) => {
           },
         },
       },
-    }
+    };
   }
-  
+
   return {
-    defaultConnection: 'default',
+    defaultConnection: "default",
     connections: {
       default: {
-        connector: 'bookshelf',
+        connector: "bookshelf",
         settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
         },
         options: {
           useNullAsDefault: true,
         },
       },
     },
-  }
+  };
 };
--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,38 @@
 module.exports = ({ env }) => {
+  // if(env('NODE_ENV') === 'production'){
+  //     return {
+  //         upload: {
+  //             provider: 'aws-s3',
+  //             providerOptions: {
+  //                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
+  //                 secretAccessKey: env('AWS_ACCESS_SECRET'),
+  //                 region: env('AWS_REGION'),
+  //                 params: {
+  //                     Bucket: env('AWS_BUCKET'),
+  //                 },
+  //             },
+  //         },
+  //     }
+  // }
 
-    if(env('NODE_ENV') === 'production'){
-        return {
-            upload: {
-                provider: 'aws-s3',
-                providerOptions: {
-                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                    secretAccessKey: env('AWS_ACCESS_SECRET'),
-                    region: env('AWS_REGION'),
-                    params: {
-                        Bucket: env('AWS_BUCKET'),
-                    },
-                },
-            },
-        }
-    }
-
-    return {
-        //Empty config which will use defaults
-    }
-}
+  return {
+    //Empty config which will use defaults
+    email: {
+      provider: "smtp",
+      providerOptions: {
+        host: env("SMTP_HOST"), //SMTP Host
+        port: env("SMTP_PORT"), //SMTP Port
+        secure: true,
+        username: env("SMTP_USERNAME"),
+        password: env("SMTP_PASSWORD"),
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1,
+      },
+      settings: {
+        from: env("SMTP_USERNAME"),
+        replyTo: env("SMTP_USERNAME"),
+      },
+    },
+  };
+};
