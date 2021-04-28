04a66a0d12665ed638622035987801732bfa791b
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-04-29 00:13:36 +0530

    add sendgrid integration for mail

--- config/plugins.js
+++ config/plugins.js
@@ -15,23 +15,36 @@ module.exports = ({ env }) => {
   //     }
   // }
 
+  // return {
+  //   //SMTP Config
+  //   email: {
+  //     provider: "smtp",
+  //     providerOptions: {
+  //       host: env("SMTP_HOST"), //SMTP Host
+  //       port: env("SMTP_PORT"), //SMTP Port
+  //       secure: true,
+  //       username: env("SMTP_USERNAME"),
+  //       password: env("SMTP_PASSWORD"),
+  //       rejectUnauthorized: true,
+  //       requireTLS: true,
+  //       connectionTimeout: 1,
+  //     },
+  //     settings: {
+  //       from: env("SMTP_USERNAME"),
+  //       replyTo: env("SMTP_USERNAME"),
+  //     },
+  //   },
+  // };
+
   return {
-    //Empty config which will use defaults
     email: {
-      provider: "smtp",
+      provider: "sendgrid",
       providerOptions: {
-        host: env("SMTP_HOST"), //SMTP Host
-        port: env("SMTP_PORT"), //SMTP Port
-        secure: true,
-        username: env("SMTP_USERNAME"),
-        password: env("SMTP_PASSWORD"),
-        rejectUnauthorized: true,
-        requireTLS: true,
-        connectionTimeout: 1,
+        apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        from: env("SMTP_USERNAME"),
-        replyTo: env("SMTP_USERNAME"),
+        defaultFrom: "vinoth@voqzi.com",
+        defaultReplyTo: "vinoth@voqzi.com",
       },
     },
   };
