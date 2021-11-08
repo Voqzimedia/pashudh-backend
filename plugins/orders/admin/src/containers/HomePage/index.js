6c487004573322a588aabb9f88241739281c8e33
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2021-11-08 12:59:34 +0530

    just tested

--- plugins/orders/admin/src/containers/HomePage/index.js
+++ plugins/orders/admin/src/containers/HomePage/index.js
@@ -4,14 +4,25 @@
  *
  */
 
-import React, { memo } from "react";
+import axios from "axios";
+import React, { memo, useEffect } from "react";
 // import PropTypes from 'prop-types';
+import {
+  CheckPagePermissions,
+  LoadingIndicatorPage,
+  NotFound,
+  request,
+} from "strapi-helper-plugin";
 import pluginId from "../../pluginId";
 // import ListView from "../../containers-new/ListView";
-import * as strapi from "strapi-helper-plugin";
+// import * as strapi from "strapi-helper-plugin";
 
 const HomePage = () => {
-  console.log(strapi);
+  // const orders = strapi.services.order.find();
+
+  useEffect(() => {}, []);
+
+  // axios.get(`${strapi?.backendURL}/orders`).then((res) => console.log(res));
 
   return <div></div>;
 };
